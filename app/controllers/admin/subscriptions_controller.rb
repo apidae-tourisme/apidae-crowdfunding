a7d3d97d9ef29fdfbcab5eb7ba6890f0ae77b593
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-12-26 09:40:26 +0100

    fix auth

--- app/controllers/admin/subscriptions_controller.rb
+++ app/controllers/admin/subscriptions_controller.rb
@@ -9,7 +9,7 @@ class Admin::SubscriptionsController < Admin::UserController
 
   def check_credentials
     authenticate_user!
-    unless Rails.application.config.widgit_admins.include?(current_user.email)
+    unless Rails.application.config.subscriptions_admins.include?(current_user.email)
       redirect_to root_url, alert: "Vous n'êtes pas autorisé(e) à accéder à cette page."
     end
   end
