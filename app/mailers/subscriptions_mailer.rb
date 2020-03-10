9bf8e37dd1ee77cf32fcaab9f28709e1a9a14e70
Jean-Baptiste Vilain <jbvilain@gmail.com>
2020-03-10 17:40:53 +0100

    replaced iban with rib wording

--- app/mailers/subscriptions_mailer.rb
+++ app/mailers/subscriptions_mailer.rb
@@ -2,11 +2,13 @@ class SubscriptionsMailer < ApplicationMailer
   def confirm_subscription(subscription)
     @subscription = subscription
     attachments['pionnier-apidae.png'] = File.read('./badges/badge-pionnier-apidae.png')
-    mail(to: @subscription.email, subject: "Confirmation de Souscription à Apidae Tourisme Scic SA à capital variable")
+    mail(to: @subscription.email, bcc: 'notification@souscription.apidae-tourisme.com',
+         subject: "Confirmation de Souscription à Apidae Tourisme Scic SA à capital variable")
   end
 
   def complete_subscription(subscription)
     @subscription = subscription
-    mail(to: @subscription.email, subject: "Confirmez votre prise de participation au capital Apidae Tourisme")
+    mail(to: @subscription.email, bcc: 'notification@souscription.apidae-tourisme.com',
+         subject: "Confirmez votre prise de participation au capital Apidae Tourisme")
   end
 end
